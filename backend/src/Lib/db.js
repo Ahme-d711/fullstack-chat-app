8d3c48d046115f825c19592941ff8c8ac5bcafe4
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-09-28 22:07:13 +0300

    update: minor improvements and code adjustments

--- backend/src/Lib/db.js
+++ backend/src/Lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const contactDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB Connected Error: ", error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -20,13 +20,13 @@ const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true, // ✅ عشان الكوكيز يتبعت
+    credentials: true, 
   })
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(cookieParser()); // ✅ مهم عشان تقدر توصل للكوكيز
+app.use(cookieParser()); 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
